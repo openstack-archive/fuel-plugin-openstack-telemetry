fc613baa5944945c1a9747440c7c641449b8c25c
Volodymyr Kornylyuk <vkornylyuk@mirantis.com>
2016-09-02 18:29:48 +0300

    Adds hindsight
    - downloads packages
    - installs packages
    - puts configs and scripts
    - starts service
    
    Change-Id: I3feaa1a4afcb016db71b76cf975f0ab8f23801d3

--- deployment_scripts/puppet/manifests/configure-influxdb.pp
+++ deployment_scripts/puppet/manifests/configure-influxdb.pp
@@ -2,7 +2,7 @@ notice('MODULAR: fuel-plugin-telemetry: influxdb-create-db.pp')
 
 $influxdb_mode = hiera('telemetry::influxdb::mode')
 
-if $influxdb_mode == 'remote' {
+if $influxdb_mode == 'local' {
 
   class { 'telemetry::create_influxdb_database': }
 
