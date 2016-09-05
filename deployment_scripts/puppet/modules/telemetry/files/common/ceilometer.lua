98d9274ddc507fe7f68e0cbda893ceb582d13abf
Ilya Tyaptin <ityaptin@mirantis.com>
2016-09-05 19:13:56 +0300

    Add lua plugins for both Hindsight and Heka
    In `common` folder lua code shared between Heka and Hindsight
    are stored.
    All hindsight specific code stored in hindsight folder with correct
    hierarchy for hindsight sandbox `run` directory.
    
    This CR provides only samples collecting feature.
    
    Change-Id: Ie1675673986462b7de3bdf1757f494a53424928f

---
+++ deployment_scripts/puppet/modules/telemetry/files/common/ceilometer.lua
@@ -0,0 +1,54 @@
+-- Copyright 2016 Mirantis, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local table = require 'table'
+
+local samples = require 'samples'
+local patt = require 'patterns'
+local utils = require 'lma_utils'
+
+local l = require 'lpeg'
+l.locale(l)
+
+local fields_grammar = l.Ct((l.C((l.P(1) - l.P" ")^1) * l.P" "^0)^0)
+local metadata_fields = fields_grammar:match(
+    read_config("metadata_fields") or ""
+)
+local samples_decoder = samples.new(metadata_fields)
+
+local decode_resources = false
+resource_decoder = {}
+
+local CeilometerDecoder = {}
+CeilometerDecoder.__index = CeilometerDecoder
+
+setfenv(1, CeilometerDecoder) -- Remove external access to contain everything in the module
+
+function inject(code, msg)
+    if code == 0 and msg then
+        return utils.safe_inject_message(msg)
+    else
+        return code, msg
+    end
+end
+
+function decode(data)
+    local code, msg = inject(samples_decoder:decode(data))
+    if code == 0 and decode_resources then
+        code, msg = inject(resource_decoder:decode(data))
+    end
+    return code, msg
+end
+
+return CeilometerDecoder
