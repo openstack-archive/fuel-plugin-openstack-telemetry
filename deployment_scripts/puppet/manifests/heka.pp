17d7e3da178dacfb198e2d6ded4b2e9a2d6cdda9
Volodymyr Kornylyuk <vkornylyuk@mirantis.com>
2016-09-13 00:19:20 +0300

    Kafka support
    - get kafka IPs
    - configure ceilometer.conf
    - configure hindsight configs
    - deploy hindsight on Kafka nodes
    - disable heka when Kafka detected
    
    Change-Id: I3df403affab6f8427141c19a07a3312af7e92630

--- deployment_scripts/puppet/manifests/heka.pp
+++ deployment_scripts/puppet/manifests/heka.pp
@@ -1,118 +1,125 @@
 
-## Get values
+notice('MODULAR: fuel-plugin-telemetry: heka.pp')
 
-$config_dir      = hiera('telemetry::heka::config_dir')
-$amqp_url        = hiera('telemetry::rabbit::url')
-$metadata_fields = hiera('telemetry::metadata_fields')
+if !hiera('telemetry::kafka::enabled') {
 
-if hiera('telemetry::elasticsearch::server',false) {
-  $ip = hiera('telemetry::elasticsearch::server')
-  $port = hiera('telemetry::elasticsearch::server')
-  $elasticsearch_url = "http://${ip}:${port}"
-} else {
-  #no Elasticsearch
-  #heka failed to start if url schemma is not valid, so we set http here
-  $elasticsearch_url = 'http://'
-}
+  ## Get values
 
-$influxdb_address  = hiera('telemetry::influxdb::address')
-$influxdb_port     = hiera('telemetry::influxdb::port')
-$influxdb_database = hiera('telemetry::influxdb::database')
-$influxdb_user     = hiera('telemetry::influxdb::user')
-$influxdb_password = hiera('telemetry::influxdb::password')
+  $config_dir      = hiera('telemetry::heka::config_dir')
+  $amqp_url        = hiera('telemetry::rabbit::url')
+  $metadata_fields = hiera('telemetry::metadata_fields')
 
-$modules_dir       = hiera('telemetry::lua::modules_dir')
 
-### Heka configuration
+  if hiera('telemetry::elasticsearch::server',false) {
+    $ip = hiera('telemetry::elasticsearch::server')
+    $port = hiera('telemetry::elasticsearch::server')
+    $elasticsearch_url = "http://${ip}:${port}"
+  } else {
+    #no Elasticsearch
+    #heka failed to start if url schemma is not valid, so we set http here
+    $elasticsearch_url = 'http://'
+  }
 
-File {
-  before => Service['telemetry-collector']
-}
+  $influxdb_address  = hiera('telemetry::influxdb::address')
+  $influxdb_port     = hiera('telemetry::influxdb::port')
+  $influxdb_database = hiera('telemetry::influxdb::database')
+  $influxdb_user     = hiera('telemetry::influxdb::user')
+  $influxdb_password = hiera('telemetry::influxdb::password')
 
-file {
-  "${config_dir}/amqp-openstack_sample.toml":              content => template( 'telemetry/heka/amqp-openstack_sample.toml.erb' );
-  "${config_dir}/decoder-sample.toml":                     content => template( 'telemetry/heka/decoder-sample.toml.erb' );
-  "${config_dir}/encoder-influxdb.toml":                   content => template( 'telemetry/heka/encoder-influxdb.toml.erb' );
-  "${config_dir}/encoder-resource-elasticsearch.toml":     content => template( 'telemetry/heka/encoder-resource-elasticsearch.toml.erb' );
-  "${config_dir}/file-output-resource.toml":               content => template( 'telemetry/heka/file-output-resource.toml.erb' );
-  "${config_dir}/file-output.toml":                        content => template( 'telemetry/heka/file-output.toml.erb' );
-  "${config_dir}/filter-influxdb_accumulator_sample.toml": content => template( 'telemetry/heka/filter-influxdb_accumulator_sample.toml.erb' );
-  # TODO disable config when Elasticsearch not in use
-  "${config_dir}/output-elasticsearch-resource2.toml":     content => template( 'telemetry/heka/output-elasticsearch-resource2.toml.erb' );
-  "${config_dir}/output-influxdb-samples.toml":            content => template( 'telemetry/heka/output-influxdb-samples.toml.erb' );
-}
+  $modules_dir       = hiera('telemetry::lua::modules_dir')
 
-# Heka Installation
+  ### Heka configuration
 
-$version            = hiera('telemetry::heka::version')
-$max_message_size   = hiera('telemetry::heka::max_message_size')
-$max_process_inject = hiera('telemetry::heka::max_process_inject')
-$max_timer_inject   = hiera('telemetry::heka::max_timer_inject')
-$poolsize           = hiera('telemetry::heka::poolsize')
+  File {
+    before => Service['telemetry-collector']
+  }
 
-# TODO we dont't need them on controller
-$install_init_script = false
+  file {
+    "${config_dir}/amqp-openstack_sample.toml":              content => template( 'telemetry/heka/amqp-openstack_sample.toml.erb' );
+    "${config_dir}/decoder-sample.toml":                     content => template( 'telemetry/heka/decoder-sample.toml.erb' );
+    "${config_dir}/encoder-influxdb.toml":                   content => template( 'telemetry/heka/encoder-influxdb.toml.erb' );
+    "${config_dir}/encoder-resource-elasticsearch.toml":     content => template( 'telemetry/heka/encoder-resource-elasticsearch.toml.erb' );
+    "${config_dir}/file-output-resource.toml":               content => template( 'telemetry/heka/file-output-resource.toml.erb' );
+    "${config_dir}/file-output.toml":                        content => template( 'telemetry/heka/file-output.toml.erb' );
+    "${config_dir}/filter-influxdb_accumulator_sample.toml": content => template( 'telemetry/heka/filter-influxdb_accumulator_sample.toml.erb' );
+    # TODO disable config when Elasticsearch not in use
+    "${config_dir}/output-elasticsearch-resource2.toml":     content => template( 'telemetry/heka/output-elasticsearch-resource2.toml.erb' );
+    "${config_dir}/output-influxdb-samples.toml":            content => template( 'telemetry/heka/output-influxdb-samples.toml.erb' );
+  }
 
-# Workaround for heka module
-# to prevent changing home of root user
-$user='root'
-user { $user: }
+  # Heka Installation
 
-::heka { 'telemetry-collector':
-  config_dir          => '/etc/telemetry-collector',
-  user                => $user,
-  #additional_groups   => $additional_groups,
-  hostname            => $::hostname,
-  max_message_size    => $max_message_size,
-  max_process_inject  => $max_process_inject,
-  max_timer_inject    => $max_timer_inject,
-  poolsize            => $poolsize,
-  install_init_script => $install_init_script,
-  version             => $version,
-}
+  $version            = hiera('telemetry::heka::version')
+  $max_message_size   = hiera('telemetry::heka::max_message_size')
+  $max_process_inject = hiera('telemetry::heka::max_process_inject')
+  $max_timer_inject   = hiera('telemetry::heka::max_timer_inject')
+  $poolsize           = hiera('telemetry::heka::poolsize')
 
-# Heka pacemaker config
+  # TODO we dont't need them on controller
+  $install_init_script = false
 
-pacemaker::service { 'telemetry-collector':
-  ensure           => present,
-  prefix           => false,
-  primitive_class  => 'ocf',
-  primitive_type   => 'ocf-telemetry',
-  use_handler      => false,
-  complex_type     => 'clone',
-  complex_metadata => {
-    # the resource should start as soon as the dependent resources
-    # (eg RabbitMQ) are running *locally*
-    'interleave' => true,
-  },
-  metadata         => {
-    # Make sure that Pacemaker tries to restart the resource if it fails
-    # too many times
-    'failure-timeout'     => '120s',
-    'migration-threshold' => '3',
-  },
-  parameters       => {
-    'service_name' => 'telemetry-collector',
-    'config'       => '/etc/telemetry-collector',
-    'log_file'     => '/var/log/telemetry-collector.log',
-    'user'         => $user,
-  },
-  operations       => {
-    'monitor' => {
-      'interval' => '20',
-      'timeout'  => '10',
+  # Workaround for heka module
+  # to prevent changing home of root user
+  $user='root'
+  user { $user: }
+
+  ::heka { 'telemetry-collector':
+    config_dir          => '/etc/telemetry-collector',
+    user                => $user,
+    #additional_groups   => $additional_groups,
+    hostname            => $::hostname,
+    max_message_size    => $max_message_size,
+    max_process_inject  => $max_process_inject,
+    max_timer_inject    => $max_timer_inject,
+    poolsize            => $poolsize,
+    install_init_script => $install_init_script,
+    version             => $version,
+  }
+
+  # Heka pacemaker config
+
+  pacemaker::service { 'telemetry-collector':
+    ensure           => present,
+    prefix           => false,
+    primitive_class  => 'ocf',
+    primitive_type   => 'ocf-telemetry',
+    use_handler      => false,
+    complex_type     => 'clone',
+    complex_metadata => {
+      # the resource should start as soon as the dependent resources
+      # (eg RabbitMQ) are running *locally*
+      'interleave' => true,
     },
-    'start'   => {
-      'timeout' => '30',
+    metadata         => {
+      # Make sure that Pacemaker tries to restart the resource if it fails
+      # too many times
+      'failure-timeout'     => '120s',
+      'migration-threshold' => '3',
     },
-    'stop'    => {
-      'timeout' => '30',
+    parameters       => {
+      'service_name' => 'telemetry-collector',
+      'config'       => '/etc/telemetry-collector',
+      'log_file'     => '/var/log/telemetry-collector.log',
+      'user'         => $user,
     },
-  },
-}
+    operations       => {
+      'monitor' => {
+        'interval' => '20',
+        'timeout'  => '10',
+      },
+      'start'   => {
+        'timeout' => '30',
+      },
+      'stop'    => {
+        'timeout' => '30',
+      },
+    },
+  }
 
-service { 'telemetry-collector':
-  ensure   => 'running',
-  enable   => true,
-  provider => 'pacemaker',
+  service { 'telemetry-collector':
+    ensure   => 'running',
+    enable   => true,
+    provider => 'pacemaker',
+  }
+
 }
