d31c604affb4051d029b715491cdb9bea4bd8d52
Volodymyr Kornylyuk <vkornylyuk@mirantis.com>
2016-09-19 11:41:15 +0300

    Creates topics
    - creates Kafka topics: notifications.sample and metering.sample
    
    Change-Id: I6cb6abd8f0f121c477bdeb1f5e436a651ba77226

---
+++ deployment_scripts/puppet/manifests/create-topics.pp
@@ -0,0 +1,36 @@
+
+notice('MODULAR: fuel-plugin-telemetry: create-topics.pp')
+
+# Topic settings
+
+prepare_network_config(hiera_hash('network_scheme', {}))
+$network_metadata         = hiera_hash('network_metadata')
+$controllers              = get_nodes_hash_by_roles($network_metadata, ['controller', 'primary-controller'])
+$controllers_amount       = count($controllers)
+$notifications_partitions = max($controllers_amount/3,1)*$::processorcount
+$metering_partitions      = $controllers_amount
+
+# Connection info
+
+$brokerlist         = hiera('telemetry::kafka::nodes_list')
+$replication_factor = count($brokerlist)
+$zookeeper_ip       = $brokerlist[0]
+$zookeeper_address  = "${zookeeper_ip}:2181"
+$script_location    = '/tmp/create-topics.sh'
+
+file { $script_location:
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0740',
+  content => template('telemetry/create-topics.sh.erb'),
+}
+
+exec { "run_${script_location}":
+  command => $script_location,
+  require => File[$script_location],
+}
+
+exec { "remove_${script_location}":
+  command => "/bin/rm -f ${script_location}",
+  require => Exec["run_${script_location}"],
+}
