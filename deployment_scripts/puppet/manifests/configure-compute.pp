83486324fb58cf6e27073bc6f3f6fb2825ba0c8a
Volodymyr Kornylyuk <vkornylyuk@mirantis.com>
2016-09-21 13:26:04 +0300

    Ceilometer config on compute, other fixes
    - kafka url on compute
    - oslo package update
    - create topics before ceilometer start
    
    Change-Id: I6af7e9a2bc88dbe51db9bbf45fcbce7f44893a41

---
+++ deployment_scripts/puppet/manifests/configure-compute.pp
@@ -0,0 +1,17 @@
+
+notice('MODULAR: fuel-plugin-telemetry: configure-compute.pp')
+
+if hiera('telemetry::kafka::enabled') {
+
+  $kafka_ips  = hiera('telemetry::kafka::broker_list')
+  $kafka_url  = "moskafka://${kafka_ips}"
+  ceilometer_config {
+    'oslo_messaging_notifications/transport_url': value => $kafka_url;
+    'oslo_messaging_kafka/consumer_group':        value => 'ceilometer';
+    'DEFAULT/transport_url':                      value => $kafka_url;
+    'DEFAULT/shuffle_time_before_polling_task':   value => 300;
+    'compute/resource_update_interval':           value => 600;
+  } ~>
+  service { 'ceilometer-polling':}
+
+}
