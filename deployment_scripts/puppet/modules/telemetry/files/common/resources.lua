5be6ddf5032c9b1a9743d9f6fa6b788c950edbe9
Ildar Svetlov <isvetlov@mirantis.com>
2017-01-27 18:16:03 +0400

    Timestamp transformtion to uniform format for ES
    Transform timestamp values to uniform format
    with 'T' in the middle for ES resources.
    
    Change-Id: Iffe442c6f855f30fb7bbbe9c39399a0bfd35db11

--- deployment_scripts/puppet/modules/telemetry/files/common/resources.lua
+++ deployment_scripts/puppet/modules/telemetry/files/common/resources.lua
@@ -14,8 +14,6 @@
 
 local cjson = cjson
 local string = string
-local table = table
-local math = math
 local setmetatable = setmetatable
 local ipairs = ipairs
 local pairs = pairs
@@ -31,7 +29,6 @@ function normalize_uuid(uuid)
     return patt.Uuid:match(uuid)
 end
 
-local metadata_fields = {}
 
 local ResourcesDecoder = {}
 ResourcesDecoder.__index = ResourcesDecoder
@@ -67,11 +64,23 @@ local resource_msg = {
     Payload = nil
 }
 
+function transform_metadata(metadata)
+    if type(metadata) == 'table' then
+        for name, value in ipairs(metadata) do
+            local transform = transform_functions[name]
+            if transform ~= nil then
+                metadata[name] = transform(value)
+            end
+        end
+    end
+end
+
 function add_resource_to_payload(sample, payload)
     local counter_name, _ = string.gsub(sample.counter_name, "%.", "\\")
 
+    transform_metadata(sample.resource_metadata)
     local resource_data = {
-        timestamp = sample.timestamp,
+        timestamp = utils.format_datetime(sample.timestamp),
         resource_id = sample.resource_id,
         source = sample.source or "",
         metadata = sample.resource_metadata,
