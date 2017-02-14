914c33d3c805c109df2096b6ca1e7b7f97ad14bb
Ildar Svetlov <isvetlov@mirantis.com>
2017-02-14 14:31:46 +0400

    Remove empty fields from resource metadata
    Change-Id: I6d8b1678e2e26e541d41d5894890cad57f4488a8

--- deployment_scripts/puppet/modules/telemetry/files/common/resources.lua
+++ deployment_scripts/puppet/modules/telemetry/files/common/resources.lua
@@ -68,20 +68,27 @@ function add_resource_to_payload(sample, payload)
     local counter_name, _ = string.gsub(sample.counter_name, "%.", "\\")
 
     local metadata = sample.resource_metadata
+    local local_metadata = {}
+
     if type(metadata) == 'table' then
         for name, value in ipairs(metadata) do
             local transform = transform_functions[name]
             if transform ~= nil then
                 metadata[name] = transform(value)
             end
+            if metadata[name] ~= '' then
+                local_metadata[name] = metadata[name]
+            end
         end
+    else
+        local_metadata = metadata
     end
 
     local resource_data = {
         timestamp = utils.format_datetime(sample.timestamp),
         resource_id = sample.resource_id,
         source = sample.source or "",
-        metadata = sample.resource_metadata,
+        metadata = local_metadata,
         user_id = sample.user_id,
         project_id = sample.project_id,
         meter = {
